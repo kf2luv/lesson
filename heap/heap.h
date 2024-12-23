c95e4e61f29bbc44add035da74cbd74f0666db5e
kf2luv <15207683889@163.com>
2024-12-23 09:29:01 +0000

    heap2

--- heap/heap.h
+++ heap/heap.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -5,15 +6,18 @@
 
 namespace ckf
 {
+    // 模板指定类型和比较器
+    template <class T, class Comparison>
     class Heap
     {
     public:
-        // 大根堆（完全二叉树）
-        Heap(const std::vector<int> &arr);
-
-        int top();
+        Heap(const std::vector<T> &arr);
+        Heap();
+        T top();
+        size_t size();
+        bool empty();
         void pop();
-        void push(int value);
+        void push(const T &value);
 
         void show();
 
@@ -27,6 +31,135 @@ namespace ckf
         void adjustUp(size_t pos);
 
     private:
-        std::vector<int> _arr;
+        std::vector<T> _arr;
+        Comparison _comp; // 比较器
     };
 } // ckf
+
+template <class T, class Comparison>
+ckf::Heap<T, Comparison>::Heap(const std::vector<T> &arr)
+    : _arr(arr)
+{
+    //将传入的序列堆化
+    heapify();
+}
+
+template <class T, class Comparison>
+ckf::Heap<T, Comparison>::Heap()
+{
+}
+
+template <class T, class Comparison>
+inline T ckf::Heap<T, Comparison>::top()
+{
+    assert(!empty());
+    return _arr.front();
+}
+
+template <class T, class Comparison>
+inline size_t ckf::Heap<T, Comparison>::size()
+{
+    return _arr.size();
+}
+
+template <class T, class Comparison>
+inline bool ckf::Heap<T, Comparison>::empty()
+{
+    return size() == 0;
+}
+
+template <class T, class Comparison>
+void ckf::Heap<T, Comparison>::pop()
+{
+    assert(!empty());
+    // 交换首尾元素，删掉尾，然后从根节点向下调整
+    std::swap(_arr.front(), _arr.back());
+    _arr.pop_back();
+    if (empty())
+    { // 只有一个元素，就不用向下调整了
+        return;
+    }
+    adjustDown(0);
+}
+
+template <class T, class Comparison>
+void ckf::Heap<T, Comparison>::push(const T &value)
+{
+    _arr.push_back(value);
+    adjustUp(_arr.size() - 1);
+}
+
+template <class T, class Comparison>
+void ckf::Heap<T, Comparison>::heapify()
+{
+    // 从最后一个父节点开始，向下调整
+    for (int root = (_arr.size() - 1 - 1) >> 1; root >= 0; root--)
+    {
+        adjustDown(root);
+    }
+}
+
+template <class T, class Comparison>
+void ckf::Heap<T, Comparison>::adjustDown(size_t root)
+{
+    int n = _arr.size();
+    assert(root >= 0 && root < n);
+
+    int pos = root;
+    int largest = pos;
+    while (pos < n)
+    {
+        int leftChild = (pos << 1) + 1;
+        int rightChild = leftChild + 1;
+
+        // 得出root/left/right三者最大的节点largest
+        if (leftChild < n && _comp(_arr[largest], _arr[leftChild]))
+        {
+            // leftChild满足条件，往上走
+            largest = leftChild;
+        }
+        if (rightChild < n && _comp(_arr[largest], _arr[rightChild]))
+        {
+            // rightChild满足条件，往上走
+            largest = rightChild;
+        }
+
+        if (largest == pos)
+        {
+            // 最大值就是当前pos，不用调整了
+            return;
+        }
+
+        std::swap(_arr[pos], _arr[largest]);
+        pos = largest;
+    }
+}
+
+template <class T, class Comparison>
+void ckf::Heap<T, Comparison>::adjustUp(size_t pos)
+{
+    while (pos > 0)
+    {
+        int parent = (pos - 1) >> 1;
+        if (parent >= 0 && _comp(_arr[parent], _arr[pos]))
+        {
+            // pos满足条件，往上走
+            std::swap(_arr[pos], _arr[parent]);
+            pos = parent;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+template <class T, class Comparison>
+void ckf::Heap<T, Comparison>::show()
+{
+    for (auto x : _arr)
+    {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+}
--- heap/test.cpp
+++ heap/test.cpp
@@ -1,28 +1,55 @@
 #include "heap.h"
 
-void testCreateHeap()
+// void testCreateHeap()
+// {
+//     ckf::Heap h1({1, 8, 3, 9, 4, 5, 6});
+//     h1.show();
+
+//     ckf::Heap h2({8, 94, 25, 59, 5, 57, 14, 34, 53, 61});
+//     h2.show();
+// }
+
+// void testPushPop(){
+//     ckf::Heap h1({1, 8, 3, 9, 4, 5, 6});
+//     h1.show();
+
+//     h1.push(7);
+//     h1.push(10);
+//     h1.show();
+
+//     h1.pop();
+//     h1.show();
+// }
+
+class MyComp1
 {
-    ckf::Heap h1({1, 8, 3, 9, 4, 5, 6});
-    h1.show();
+public:
+    bool operator()(const int &lhs, const int &rhs)
+    {
+        return lhs < rhs; // 控制堆中元素大的往上走
+    }
+};
 
-    ckf::Heap h2({8, 94, 25, 59, 5, 57, 14, 34, 53, 61});
-    h2.show();
-}
+class MyComp2
+{
+public:
+    bool operator()(const int &lhs, const int &rhs)
+    {
+        return lhs > rhs; // 控制堆中元素小的往上走
+    }
+};
 
-void testPushPop(){
-    ckf::Heap h1({1, 8, 3, 9, 4, 5, 6});
-    h1.show();
+//符合条件时（返回值为true），比较器comp的第二个参数往上走
 
-    h1.push(7);
-    h1.push(10);
-    h1.show();
-
-    h1.pop();
-    h1.show();
+void testTemplateHeap()
+{
+    ckf::Heap<int, MyComp2> heap({1, 8, 3, 9, 4, 5, 6});
+    heap.show();
 }
 
 int main()
 {
     // testCreateHeap();
-    testPushPop();
+    // testPushPop();
+    testTemplateHeap();
 }
